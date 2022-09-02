ce4ad31c63de826b81aaeb3b5227ac94bd936f2a
walisonls <gwalisonls@gmail.com>
2022-09-02 16:35:53 -0300

    Up

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <router-link to="/">
+      <img src="" id="logo">
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "NavBar"
+  }
+</script>
+
+<style scoped>
+
+</style>
