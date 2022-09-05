51370552a7259784c9dbfdb2199178519fa6b6ce
walisonls <gwalisonls@gmail.com>
2022-09-05 13:11:02 -0300

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <Banner />
   <div class="main-container">
     <h1>Monte o seu burgeur:</h1>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
-  import Banner from '../components/Banner.vue'
+  import Banner from '../components/Landing.vue'
+  import BurgerForm from '../components/BurgerForm.vue'
 
   export default {
     name: 'Home',
     components: {
-      Banner
-    }
+    Banner,
+    BurgerForm,
+}
   }
 </script>
 
